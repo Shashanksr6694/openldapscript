cb1f8f09ad53b53de6f4ec27d585b9c68df170f5
root <root@ip-172-31-3-85.ap-south-1.compute.internal>
2018-02-16 10:40:15 +0000

    new

---
+++ addldapuser.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+LDAPADDCMD="ldapadd"
+LDAPDELCMD="ldapdelete"
+SLAPPASSWORD="slappasswd"
+SECRET="redhat"
+LDAPPASS="crevise"
+HOMEDIR="/home/users"
+SKEL="/etc/skel"
+TMP="/home/ubuntu/openldapscript/tmp/"
+DOMAIN="dc=ec2-13-127-170-246,dc=ap-south-1,dc=compute,dc=amazonaws,dc=com"
+
+
+if [ -z $1 ];  then
+	echo "addldapuser.sh <username>"
+	exit 1
+fi
+
+USERNAME=$1
+
+stty_orig=`stty -g`
+echo -n "Enter Password: "
+stty -echo
+read USERPASS
+stty $stty_orig
+
+echo
+PASSWORD=`$SLAPPASSWORD -h "{crypt}" -s $USERPASS`
+
+LUID=`echo $[ 1000 + $[ RANDOM % 65535 ]]`
+
+(
+cat <<add-user
+dn: cn=$USERNAME,ou=users,$DOMAIN
+objectClass: top
+objectClass: account
+objectClass: posixAccount
+objectClass: shadowAccount
+cn: $USERNAME
+uid: $USERNAME
+uidNumber: $LUID
+gidNumber: $LUID
+userPassword: $PASSWORD
+loginShell: /bin/bash
+homeDirectory: /home/users/$USERNAME
+add-user
+) > $TMP/adduser.ldif
+
+$LDAPADDCMD -x -w $LDAPPASS -D "cn=admin,$DOMAIN" -f $TMP/adduser.ldif && rm $TMP/adduser.ldif 
+
+if [ $? -ne "0" ]; then
+	echo "Add user failed"
+	echo "Please review $TMP/adduser.ldif and add the account manually"
+else
+#	mkdir -p $HOMEDIR/$USERNAME
+	cp -Rv $SKEL $HOMEDIR/$USERNAME
+	chown -Rv $LUID:$LUID $HOMEDIR/$USERNAME
+fi
